0283e8a3d1ae39b7b355d3359248718cabbbf1d8
Valery <valerygusev2022@gmail.com>
2023-03-25 16:24:36 +0300

    changes

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const notesArea = document.getElementById("notesArea");
-
 const addBtn = document.querySelector(".addBtn");
 
+// Note structure
 function createNote() {
   const wrapper = document.createElement("div");
   wrapper.classList.add("wrapper");
@@ -21,12 +21,17 @@ function createNote() {
   const btns = document.createElement("div");
   btns.classList.add("btns");
 
-  const checkBtn = document.createElement("div");
+  const checkBtn = document.createElement("input");
+
   checkBtn.setAttribute("id", "checkBtn");
   checkBtn.classList.add("checkBtn");
+  checkBtn.setAttribute("type", "checkbox");
 
-  const delBtn = document.createElement("div");
+  const delBtn = document.createElement("button");
   delBtn.classList.add("delBtn");
+  const delImg = document.createElement("img");
+  delImg.classList.add("delImg");
+  delImg.setAttribute("src", "./../delete1.png");
 
   const cardDescr = document.createElement("textarea");
   cardDescr.classList.add("cardDescr");
@@ -34,7 +39,7 @@ function createNote() {
   cardDescr.setAttribute("rows", "1");
   cardDescr.setAttribute("placeholder", "...");
 
-  notesArea.appendChild(wrapper);
+  notesArea.prepend(wrapper);
   wrapper.appendChild(card);
 
   card.appendChild(cardHeader);
@@ -45,6 +50,8 @@ function createNote() {
 
   btns.appendChild(checkBtn);
   btns.appendChild(delBtn);
+
+  delBtn.appendChild(delImg);
 }
 // Create new note
 addBtn.addEventListener("click", () => createNote());
@@ -52,28 +59,26 @@ addBtn.addEventListener("click", () => createNote());
 // Delete note
 document.addEventListener("click", (e) => {
   const delNote = e.target.closest(".wrapper");
-  e.target.className === "delBtn" ? delNote.remove() : null;
+  e.target.className === "delImg" ? delNote.remove() : null;
 });
 
 // Check note
 
+/*
+
 document.addEventListener("click", (e) => {
   const checkBtn = e.target;
   const note = e.target.closest(".wrapper");
-  const input = e.target.closest(".title");
-  const cardDescr = e.target.closest(".cardDescr");
 
   if (e.target.id === "checkBtn") {
-    note.setAttribute("background-color", "var(--done-note)");
-    input.setAttribute("background-color", "var(--done-note)");
-    cardDescr.setAttribute("background-color", "var(--done-note)");
-    checkBtn.setAttribute("background-image", "url(./done.png);");
+    note.style.backgroundColor = "var(--done-note)";
+
     checkBtn.id = "done";
-  } else if (e.target.className === "done") {
-    note.setAttribute("background-color", "#fff");
-    input.setAttribute("background-color", "#fff");
-    cardDescr.setAttribute("background-color", "#fff");
-    checkBtn.setAttribute("background-image", "url(./check.png);");
+  } else if (e.target.id === "done") {
+    note.style.backgroundColor = "#fff";
+
     checkBtn.id = "checkBtn";
   }
 });
+
+*/
